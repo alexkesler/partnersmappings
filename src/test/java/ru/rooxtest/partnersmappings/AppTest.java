2a09f67e05e2c16415681526ca9f68e004650384
Alexey <kesler@mail.ru>
2018-01-27 19:05:12 +0500

    Made basic integration test

--- src/test/java/ru/rooxtest/partnersmappings/AppTest.java
+++ src/test/java/ru/rooxtest/partnersmappings/AppTest.java
@@ -1,7 +1,56 @@
 package ru.rooxtest.partnersmappings;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/WEB-INF/databaseContext.xml","/WEB-INF/securityContext.xml","/WEB-INF/partners-servlet.xml"})
+@WebAppConfiguration
 public class AppTest {
 
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
 
+    @Before
+    public  void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = wac.getServletContext();
+
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("customerController"));
+    }
+
+    @Test
+    public void givenGreetURI_whenMockMVC_thenVerifyResponse() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/customers"))
+                .andDo(print()).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        Assert.assertEquals("application/json;charset=UTF-8",
+                mvcResult.getResponse().getContentType());
+    }
 
 }
